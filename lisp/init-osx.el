a5bf0cd5038e3e753ce2e2b1fcc150b7af044ebf
rajat <shk.rajat@gmail.com>
2015-09-28 13:32:09 +0530

    Modified osx.el

--- lisp/init-osx.el
+++ lisp/init-osx.el
@@ -1,4 +1,13 @@
 (when *is-a-mac*
+  ;; http://stackoverflow.com/questions/20405433/how-to-force-emacs-not-use-mountain-lions-full-screen-style
+  ;; Auto hide the menu bar
+  (setq ns-auto-hide-menu-bar nil)
+  ;; Dont use the native full screen method
+  (setq ns-use-native-fullscreen nil)
+
+  ;; Anti alias the text in Mac OS
+  (setq ns-antialias-text t)
+
   ;; set keys for Apple keyboard, for emacs in OS X
   (setq mac-option-key-is-meta nil)
   (setq mac-command-key-is-meta t)
@@ -7,6 +16,9 @@
   (setq mac-option-modifier 'super)      ; make opt key do Super
   (setq mac-control-modifier 'control)   ; make Control key do Control
   ;; (setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
+
+  ;; Allow mac anti aliasing
+  (setq mac-allow-anti-aliasing t)
 
   (setq default-input-method "MacOSX")
   ;; Make mouse wheel / trackpad scrolling less jerky
@@ -23,6 +35,5 @@
     (define-key nxml-mode-map (kbd "M-h") nil))
   (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
   )
-
 
 (provide 'init-osx)
